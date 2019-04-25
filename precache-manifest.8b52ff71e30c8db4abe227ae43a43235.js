14efb49c0f6a7ad58edb3e6ab1696db5e856922b
Jonathan Johansson <jonathan.johansson@hyperlab.se>
2019-04-25 18:29:34 +0200

    Updates

---
+++ precache-manifest.8b52ff71e30c8db4abe227ae43a43235.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd567ceecc3e2cb6850bfee647599e8",
+    "url": "/pussel/index.html"
+  },
+  {
+    "revision": "1ce3e380300e064e1c68",
+    "url": "/pussel/static/css/main.fc801adf.chunk.css"
+  },
+  {
+    "revision": "3a292a1660309faa031e",
+    "url": "/pussel/static/js/2.11a12481.chunk.js"
+  },
+  {
+    "revision": "1ce3e380300e064e1c68",
+    "url": "/pussel/static/js/main.c2f26baa.chunk.js"
+  },
+  {
+    "revision": "f7fd98055e1d3e04d9e0",
+    "url": "/pussel/static/js/runtime~main.006035b0.js"
+  }
+]);
